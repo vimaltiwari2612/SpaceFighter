c46c8f6f3db459716c9d2c9b8548ad7a2a68fd28
Vimal Tiwari <vimaltiwari2612@gmail.com>
2022-10-27 16:36:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 let lastPaintTime = 0;
 let speed = 12;
+
+let gameboardDiamension = document.getElementById("gameboard");
 let planeLocation = {
   x: 100,
-  y: -10,
+  y: 10,
 };
+
 let bullets = [];
 let aliens = [];
 let killedArmy = 0;
 let bulletsUsed = 0;
 let bulletFired = 0;
-let leftLimit = -21;
-let rightLimit = -1;
+let leftLimit = 0;
+let rightLimit = gameboardDiamension.clientWidth / 48;
 let bulletDisappearLimit = 0;
 
 createAliensArmy = () => {
-  for (var x = 1; x <=5; x++) {
-    for (var y = rightLimit; y > leftLimit +3; y--) {
+  for (var x = 1; x <= 5; x++) {
+    for (var y = leftLimit; y < rightLimit; y++) {
       var alien = {
         x: x,
         y: y,
@@ -99,9 +102,21 @@ hit = () => {
   });
 };
 
-updateGameStats = () =>{
+moveLeft = () => {
+  if (planeLocation.y > leftLimit) {
+    planeLocation.y -= 1;
+  }
+}
+
+moveRight = () => {
+  if (planeLocation.y < rightLimit) {
+    planeLocation.y += 1;
+  }
+}
+
+updateGameStats = () => {
   var gameStats = document.getElementById("gameStats");
-  gameStats.innerHTML = " Bullet Fired : "+bulletFired+" | Hit : "+ bulletsUsed + " | Kills : "+killedArmy;
+  gameStats.innerHTML = " Bullet Fired : " + bulletFired + " | Hit : " + bulletsUsed + " | Kills : " + killedArmy;
 }
 
 paintAliens = (board) => {
@@ -110,10 +125,10 @@ paintAliens = (board) => {
   });
 };
 let fireOption = prompt("Welcome to Space Fighter!!\n\nHow To Play?\n1. Move left by Left Arrow Key\n2. Move right with Right Arrow Key\n3. Fire using Arrow Up.\n\nHow would you like to fire? choose a Number.\n1. Auto\n2. Manual");
-console.log(fireOption);
+1
 refreshBoard = () => {
-  if(aliens.length == bulletsUsed){
-    alert('Game Over!!\n Hit Refresh to Play again.');
+  if (aliens.length == bulletsUsed) {
+    alert('Game Over!!\n\n' + document.getElementById("gameStats").innerHTML + '\n Hit Refresh to Play again.');
     return;
   }
   updateGameStats();
@@ -121,7 +136,7 @@ refreshBoard = () => {
   var board = getBoard();
   paintAliens(board);
   createPlane(board);
-  if(fireOption == 1)
+  if (fireOption == 1)
     createBullet();
   fire(board);
 };
@@ -146,15 +161,11 @@ window.addEventListener("keydown", (e) => {
   switch (e.key) {
 
     case "ArrowLeft":
-      if (planeLocation.y > leftLimit) {
-        planeLocation.y -= 1;
-      }
+      moveLeft();
       break;
 
     case "ArrowRight":
-      if (planeLocation.y < rightLimit) {
-        planeLocation.y += 1;
-      }
+      moveRight();
       break;
 
     case "ArrowUp":
@@ -165,3 +176,55 @@ window.addEventListener("keydown", (e) => {
       break;
   }
 });
+
+
+//for mobile
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches || // browser API
+    evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) {
+      /* right swipe */
+      moveRight();
+    } else {
+      /* left swipe */
+      moveLeft();
+    }
+  } else {
+    if (yDiff > 0) {
+      /* down swipe */
+    } else {
+      /* up swipe */
+      createBullet();
+    }
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+};
